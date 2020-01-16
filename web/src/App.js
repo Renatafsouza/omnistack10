a411154c3a9259af9336b8724f37d6a465d4e1cc
Renata Souza <renatasouza@wizsolucoes.com.br>
2020-01-16 12:09:32 -0200

    Aula 03 - template statico montado

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css';
 
 
 // . Componente - Bloco isolado de HTML, CSS e JS que não interfere no restante da aplicação.
@@ -14,34 +16,115 @@ import './Sidebar.css';
 // 
 // . Propriedade - Informações que um componente PAI passa para os FILHOS. O PAI chama dentro dele os FILHOS.
 
+// React usa uma estrutura imperativa, ou seja, a aplicação entende que um estado foi alterado. Diferente de um paradigma 
+// declarativo, que você diz exatamente o que você quer no código.
+
 import Header from './Header';
 
 function App() {
+  // useEffect é usado quando quer disparar uma função 1 única vez. Nesse caso, quando o componente é montado e pronto.
+  //  Params: 1- O que (uma função)  2- Quando? (Também chamado de dependência do useEffect())
+  const [devs, setDevs] = useState([]);
   
+  const [github_username, setGitHubUsername] = useState(''); 
+  const [techs, setTechs] = useState('');
+  const [latitude, setLatitude] = useState(''); 
+  const [longitude, setLongitude] = useState(''); 
+
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {   //caso de sucesso
+        console.log(position);
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    );
+  }, []) 
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, []) 
+  
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    });
+
+    setGitHubUsername('');
+    setTechs('');
+    console.log(response.data);
+  }
+ 
+
   return (
    <div id="app">
      <aside>
        <strong>Cadastrar</strong>
-       <form>
-         <div class="input-block">
+       <form onSubmit={handleAddDev}>
+         <div className="input-block">
           <label htmlFor="github_username">Usuário do Github</label>
-          <input name="github_username" id="github_username" required/>
+          <input 
+            name="github_username" 
+            id="github_username"
+            required
+            value={github_username}
+            onChange={e => setGitHubUsername(e.target.value)}
+           />
          </div>
          
-         <div class="input-block">
+         <div className="input-block">
           <label htmlFor="techs">Tecnologias</label>
-          <input name="techs" id="techs" required/>
+          <input
+           name="techs"
+           id="techs" 
+           required
+           value={techs}
+            onChange={e => setTechs(e.target.value)} 
+           />
          </div>
 
           <div className="input-group">
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required/>
+              <input 
+              type="number"
+               name="latitude"
+                id="latitude" 
+                required 
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)} // Espero o evendo do HTML e seto no meu setLatitude. É assim que se armazena um valor de input no React.
+                />
             </div>
 
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required/>
+              <input 
+              type="number"
+               name="longitude"
+                id="longitude" 
+                required 
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+                />
             </div>
           </div>
 
@@ -52,6 +135,56 @@ function App() {
 
      </aside>
      <main>
+
+       <ul>
+         <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/20937435?s=460&v=4" alt=""/>
+              <div className="user-info">
+                <strong>Renata Francelino</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+            </header>
+            <p>Alguma Bio Aqui</p>
+            <a href="https://github.com/Renatafsouza">Acessar perfil no Github</a>
+         </li>
+
+         <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/20937435?s=460&v=4" alt=""/>
+              <div className="user-info">
+                <strong>Renata Francelino</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+            </header>
+            <p>Alguma Bio Aqui</p>
+            <a href="https://github.com/Renatafsouza">Acessar perfil no Github</a>
+         </li>
+
+         <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/20937435?s=460&v=4" alt=""/>
+              <div className="user-info">
+                <strong>Renata Francelino</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+            </header>
+            <p>Alguma Bio Aqui</p>
+            <a href="https://github.com/Renatafsouza">Acessar perfil no Github</a>
+         </li>
+
+         <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/20937435?s=460&v=4" alt=""/>
+              <div className="user-info">
+                <strong>Renata Francelino</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+            </header>
+            <p>Alguma Bio Aqui</p>
+            <a href="https://github.com/Renatafsouza">Acessar perfil no Github</a>
+         </li>
+       </ul>
 
      </main>
 
